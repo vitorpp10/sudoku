bdc05c57d2d00d6d6c547807eac1feb18db08993
Vitor <vitorprofissionalpp10@gmail.com>
2026-04-16 15:12:50 -0300

    java

---
+++ start_in_java/src/main/java/org/example/CalculoArea.java
@@ -0,0 +1,13 @@
+//dever 2
+
+package org.example;
+
+public class CalculoArea {
+    public static void main(String[] args) {
+        double raio = 3;
+        double pi = 3.14159;
+        double area = pi * (raio * raio);
+
+        System.out.println("Area = " + area);
+    }
+}
