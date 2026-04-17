795c1f63c53d579732cd67794d036103ba813e56
Vitor <vitorprofissionalpp10@gmail.com>
2026-04-17 16:00:41 -0300

    adicÃção de musica e texto

--- sudoku/app/main.cpp
+++ sudoku/app/main.cpp
@@ -3,19 +3,33 @@
 #include<SFML/Audio.hpp>
 #include<SFML/Graphics.hpp>
 
-void JanelaSudoku() {
-  sf::Window window(sf::VideoMode({800, 600}), "Sudoku");
+void Sudoku() {
+  sf::RenderWindow window(sf::VideoMode({800, 600}), "Sudoku");
   
+  const sf::Font font("../assets/PressStart2P-Regular.ttf");
+  sf::Text text(font, "Teste", 50);
+
+  sf::Music music("../assets/beyonce_fernanda.ogg");
+  if (!music.openFromFile("../assets/beyonce_fernanda.ogg")) {
+    perror("erro na musica da fernanda");
+    exit(EXIT_FAILURE);
+  }
+  music.play();
+
   while (window.isOpen()) {
     while (const std::optional event = window.pollEvent()) {
       if (event->is<sf::Event::Closed>()) 
         window.close();
     }
+    
+    window.clear();
+    window.draw(text);
+    window.display();
   }  
 }
 
 int main() {
-  JanelaSudoku();
+  Sudoku();
 
   return EXIT_SUCCESS;
 }
